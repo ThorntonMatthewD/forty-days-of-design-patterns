127842d06c544bd61f8cf0df0afd2224a9c75239
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-07-22 23:42:01 -0400

    Add Bakery class

--- factory/bakery.rb
+++ factory/bakery.rb
@@ -3,10 +3,21 @@
 require_relative 'factories/biscuit_factory'
 require_relative 'factories/scone_factory'
 
-biscuit_maker = BiscuitFactory.new
-scone_maker = SconeFactory.new
+# Penelope's bakery! We serve biscuits and scones.
+class Bakery
+  def initialize
+    @biscuit_maker = BiscuitFactory.new
+    @scone_maker = SconeFactory.new
+  end
 
-scones = (1..10).map { scone_maker.make_food }
-biscuits = (1..10).map { biscuit_maker.make_food }
-
-p scones, biscuits
+  def prepare_goods(baked_good_type)
+    case baked_good_type
+    when 'Scone'
+      (1..10).map { scone_maker.make_food }
+    when 'Biscuit'
+      (1..10).map { biscuit_maker.make_food }
+    else
+      raise 'Baked good type provided is invalid'
+    end
+  end
+end
