f6875564a10b1aff1080f8a2d7e4e6f7e97d1345
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-07-25 23:05:12 -0400

    Fix decorators

--- decorator/converters.rb
+++ decorator/converters.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'data_store'
+require_relative 'data_store'
 
 # Alters the behavior of how data is read from storage.
 class StorageDecorator < DataStore
@@ -19,15 +19,15 @@ end
 # Converts the binary in our data store into an ASCII string
 class HumanReadableStorage < StorageDecorator
   def retrieve_data
-    @data_store.retrieve_data.pack('*c')
+    @data_store.retrieve_data.map(&:chr).join
   end
 end
 
 # Converts the binary in our data store to hex
 class HexadecimalStorage < StorageDecorator
   def retrieve_data
-    @data_store.retrieve_data..map do |d|
-      d.to_s.to_i(2).to_s(16)
+    @data_store.retrieve_data.map do |d|
+      d.to_s(16)
     end
   end
 end
