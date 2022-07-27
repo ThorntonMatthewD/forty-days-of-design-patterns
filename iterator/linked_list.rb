39aa0995ba5cc4e6825bb15b44527dff3b640fbc
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-07-26 22:19:56 -0400

    Allow for appending to the linked list

--- iterator/linked_list.rb
+++ iterator/linked_list.rb
@@ -5,6 +5,24 @@ class LinkedList
   def initialize
     @head = nil
   end
+
+  def add_element(val)
+    if @head
+      locate_tail.next = Node.new(val)
+    else
+      @head = Node.new(val)
+    end
+  end
+
+  def locate_tail
+    node = @head
+
+    return node unless node.next
+
+    while (node = node.next)
+      return node unless node.next
+    end
+  end
 end
 
 # A node for use in the linked list
