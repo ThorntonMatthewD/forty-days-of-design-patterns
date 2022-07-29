fd6758eff024a2d6e76553eec8116ac5b095af7a
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-07-28 22:38:11 -0400

    Track # of challengers in dragon class

--- chain_of_responsibility/dragon.rb
+++ chain_of_responsibility/dragon.rb
@@ -5,9 +5,11 @@ class Dragon
   def initialize(power_level=9001)
     @power_level = power_level
     @alive = true
+    @challengers_faced = 0
   end
 
   def skirmish_with_challenger(challenger_power_level)
+    @challengers_faced += 1
     @alive = false unless @power_level > challenger_power_level
   end
 
