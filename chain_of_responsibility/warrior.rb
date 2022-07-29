be3c9425cc8c578970a0921954917702ea93bdb3
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-07-28 22:39:09 -0400

    Added DragonSlayer class

--- chain_of_responsibility/warrior.rb
+++ chain_of_responsibility/warrior.rb
@@ -2,8 +2,35 @@
 
 # A powerful warrior
 class Warrior
-  def initialize(power_level, friend)
+  attr_writer :next_adventurer
+
+  def initialize(power_level, name)
     @power_level = power_level
-    @friend = friend
+    @name = name
+  end
+
+  def next_adventurer(adventurer)
+    @next_adventurer = adventurer
+
+    adventurer
+  end
+
+  def slay_dragon(dragon)
+    return @next_adventurer.slay_dragon(dragon) if @next_adventurer
+
+    nil
+  end
+end
+
+# A warrior who is trained to slay dragons
+class DragonSlayer < Warrior
+  def slay_dragon(dragon)
+    dragon.skirmish_with_challenger(@power_level)
+
+    if dragon.alive?
+      super(dragon)
+    else
+      "I, #{name}, hath slain the foul beast! Quest complete!"
+    end
   end
 end
