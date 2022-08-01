190b986c989ed60ef5810cd1c8966e2c9df0aea0
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-07-31 23:29:42 -0400

    Remove cabin methods that I never used

--- singleton/cabin.rb
+++ singleton/cabin.rb
@@ -8,14 +8,5 @@ class Cabin
 
   def initialize
     @name = 'The Cozy Cabin'
-    @door_locked = false
-  end
-
-  def lock_door
-    @door_locked = true
-  end
-
-  def unlock_door
-    @door_locked = false
   end
 end
