ea3d6871c1378c7efb0da1200945fd70999a0a5b
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-08-01 09:47:47 -0400

    Add codecov gem

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source 'https://rubygems.org'
 ruby '3.0.4'
 
 group :test do
+  gem 'codecov', '~> 0'
   gem 'rspec', '~> 3.11'
   gem 'rubocop', '~> 1.31'
   gem 'rubocop-rspec', '~> 2.12'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,12 @@ RSpec.configure do |config|
   # Run coverage checks on code via simplecov
   require 'simplecov'
   SimpleCov.start
+
+  if ENV['CI']
+    require 'codecov'
+    SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
