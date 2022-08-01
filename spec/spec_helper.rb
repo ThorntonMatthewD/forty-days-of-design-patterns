afcabdc73befd3e57aa19ceff37123aba0180dd6
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-07-31 23:20:35 -0400

    Add comment for simplecov in case I forget

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  # Run coverage checks on code via simplecov
   require 'simplecov'
   SimpleCov.start
   # rspec-expectations config goes here. You can use an alternate
