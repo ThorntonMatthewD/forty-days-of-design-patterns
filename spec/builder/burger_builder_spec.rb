db4a87b5b4d096bead214d11c64dc1ea35e67586
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-08-02 23:48:17 -0400

    Fix typo

--- spec/builder/burger_builder_spec.rb
+++ spec/builder/burger_builder_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rspec'
 
-require File.expand_path('../../builder/builder_builder', __dir__)
+require File.expand_path('../../builder/burger_builder', __dir__)
 
 describe 'BurgerBuilder - Builder' do
 
