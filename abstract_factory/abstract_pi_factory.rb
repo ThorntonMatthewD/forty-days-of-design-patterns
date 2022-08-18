5219e22b23b8ff69421029b97b10fbf84e89639c
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-08-17 23:28:18 -0400

    Change how top abstract factory will operate

--- abstract_factory/abstract_pi_factory.rb
+++ abstract_factory/abstract_pi_factory.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Produces a Raspberry Pi with or without a built-in wireless chip.
+# Produces a Raspberry Pi with either a microcontroller or processor.
 class AbstractPiFactory
-  def create_pi_with_wireless_chip; end
+  def create_pi_with_microcontroller; end
 
-  def create_pi_without_wireless_chip; end
+  def create_pi_with_processor; end
 end
