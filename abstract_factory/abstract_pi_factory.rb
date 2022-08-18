f44b75c153ec48d1a5d33c4264a437df99bffce9
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-08-17 23:33:38 -0400

    Add base model abstract method

--- abstract_factory/abstract_pi_factory.rb
+++ abstract_factory/abstract_pi_factory.rb
@@ -2,6 +2,8 @@
 
 # Produces a Raspberry Pi with different characteristics.
 class AbstractPiFactory
+  def create_base_model_pi; end
+
   def create_pi_with_wireless_chip; end
 
   def create_pi_with_headers; end
