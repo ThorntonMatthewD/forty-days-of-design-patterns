749c21149c7c6a27b76324f25936d71951e815e2
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-08-17 23:35:25 -0400

    Implement factories

--- abstract_factory/pi_factory.rb
+++ abstract_factory/pi_factory.rb
@@ -1,1 +1,42 @@
 # frozen_string_literal: true
+
+require_relative 'pi_pico'
+require_relative 'pi_zero'
+
+# Creates Raspberry Pis with microcontrollers (Picos)
+class PiMicrocontrollerFactory < AbstractPiFactory
+  def create_base_model_pi
+    PiPico.new
+  end
+
+  def create_pi_with_wireless_chip
+    PiPicoW.new
+  end
+
+  def create_pi_with_headers
+    PiPicoH.new
+  end
+
+  def create_pi_with_wireless_chip_and_headers
+    PiPicoHW.new
+  end
+end
+
+# Creates Raspberry Pis with processors (Zeros)
+class PiProcessorFactory< AbstractPiFactory
+  def create_base_model_pi
+    PiZero.new
+  end
+
+  def create_pi_with_wireless_chip
+    PiZeroW.new
+  end
+
+  def create_pi_with_headers
+    PiZeroH.new
+  end
+
+  def create_pi_with_wireless_chip_and_headers
+    PiZeroHW.new
+  end
+end
