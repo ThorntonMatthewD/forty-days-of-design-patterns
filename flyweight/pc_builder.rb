40a2e25663aa75543c340180fa2527bc4e0f1a3f
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-08-21 20:30:20 -0400

    Simplify PCStore constructor

--- flyweight/pc_builder.rb
+++ flyweight/pc_builder.rb
@@ -4,10 +4,7 @@ require_relative 'gaming_pc'
 
 # The computer store. The place where everything you actually want is never in stock.
 class PCStore
-  def initialize(initial_pc_shipment)
+  def initialize
     @pc_stock = {}
-    initial_pc_shipment.each do |specs|
-      @pc_stock[get_key(specs)] = GamingPC.new(state)
-    end
   end
 end
